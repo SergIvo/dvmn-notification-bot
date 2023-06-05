7a331c981f661efea828b2a75f74cf8fc2a08f78
Sergey <Sergey@localhost.localdomain>
2023-06-05 23:46:25 +0300

    Add timestamp to requests

--- main.py
+++ main.py
@@ -4,14 +4,14 @@ from time import sleep
 from environs import Env
 
 
-def get_response(url, token):
+def get_response(url, token, params):
     try:
         response = requests.get(
             url, 
             headers = {
                 'Authorization': f'Token {token}'
             },
-            timeout = 10
+            params=params
         )
         response.raise_for_status()
         return response.json()
@@ -22,15 +22,15 @@ def get_response(url, token):
         return None
     
     
-def check_reviews(token):
+def check_reviews(token, params=None):
     url = 'https://dvmn.org/api/user_reviews/'
-    reviews = get_response(url, token)
+    reviews = get_response(url, token, params)
     return reviews
 
 
-def make_long_poll(token):
+def make_long_poll(token, params=None):
     url = 'https://dvmn.org/api/long_polling/'
-    reviews = get_response(url, token)
+    reviews = get_response(url, token, params)
     return reviews
 
 
@@ -39,7 +39,11 @@ if __name__ == '__main__':
     env.read_env()
     
     TOKEN = env('DVMN_TOKEN')
+    params = {}
     while True:
-        new_reviews = make_long_poll(TOKEN)
-        if new_reviews:
-            print(new_reviews)
+        new_reviews = make_long_poll(TOKEN, params)
+        print(new_reviews)
+        if new_reviews.get('timestamp_to_request'):
+            params['timestamp'] = new_reviews.get('timestamp_to_request')
+        elif new_reviews.get('last_attempt_timestamp'):
+            params['timestamp'] = new_reviews.get('last_attempt_timestamp')
