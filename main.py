838206dc2538804b3cdac48e9e6d0c995957ef54
Sergey <Sergey@localhost.localdomain>
2023-06-05 18:30:51 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,34 @@
+import requests
+
+from environs import Env
+
+
+def get_response(url, token):
+    response = requests.get(
+        url, 
+        headers = {
+            'Authorization': f'Token {token}'
+        }
+    )
+    response.raise_for_status()
+    return response.json()
+    
+    
+def check_reviews(token):
+    url = 'https://dvmn.org/api/user_reviews/'
+    reviews = get_response(url, token)
+    return reviews
+
+
+def make_long_poll(token):
+    url = 'https://dvmn.org/api/long_polling/'
+    reviews = get_response(url, token)
+    return reviews
+
+
+if __name__ == '__main__':
+    env = Env()
+    env.read_env()
+    
+    TOKEN = env('DVMN_TOKEN')
+    print(make_long_poll(TOKEN))
