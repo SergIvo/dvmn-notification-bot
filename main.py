63862ae4f1edb11e8ee0a6ab3b3c30ff92cd7be8
Sergey <Sergey@localhost.localdomain>
2023-06-06 19:00:38 +0300

    Move some nested code blocks to functions

--- main.py
+++ main.py
@@ -24,6 +24,26 @@ def check_reviews(token, params=None):
         return None
 
 
+def get_timestamp(new_reviews):
+    if new_reviews.get('timestamp_to_request'):
+        return new_reviews.get('timestamp_to_request')
+    elif new_reviews.get('last_attempt_timestamp'):
+        return new_reviews.get('last_attempt_timestamp')
+    else:
+        return None
+
+
+def send_notifications(bot, new_reviews):
+    attempts = new_reviews.get('new_attempts')
+        for attempt in attempts:
+            title = attempt.get('lesson_title')
+            status = 'обнаружены ошибки' if attempt.get('is_negative') else 'работа принята'
+            url = attempt.get('lesson_url')
+
+            message = f'Урок "{title}" проверен, результат - {status}.\nСсылка на урок - {url}'
+            bot.send_message(chat_id=chat_id, text=message)
+
+
 if __name__ == '__main__':
     env = Env()
     env.read_env()
@@ -34,23 +54,11 @@ if __name__ == '__main__':
     
     bot = telegram.Bot(token=tg_api_token)
     
-    params = {}
+    params = {'timestamp': None}
     while True:
-        new_reviews = make_long_poll(dvmn_token, params)
+        new_reviews = check_reviews(dvmn_token, params)
         print(new_reviews)
 
-        if new_reviews.get('timestamp_to_request'):
-            params['timestamp'] = new_reviews.get('timestamp_to_request')
-        elif new_reviews.get('last_attempt_timestamp'):
-            params['timestamp'] = new_reviews.get('last_attempt_timestamp')
-
+        params['timestamp'] = get_timestamp(new_reviews)
         if new_reviews.get('status') == 'found':
-            attempts = new_reviews.get('new_attempts')
-            for attempt in attempts:
-                title = attempt.get('lesson_title')
-                status = 'обнаружены ошибки' if attempt.get('is_negative') else 'работа принята'
-                url = attempt.get('lesson_url')
-                
-                message = f'Урок "{title}" проверен, результат - {status}.\nСсылка на урок - {url}'
-
-                bot.send_message(chat_id=chat_id, text=message)
+            send_notifications(bot, new_reviews)
